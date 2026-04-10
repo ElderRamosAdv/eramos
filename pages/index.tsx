0130d72d35223cc560156438f6fc1dea9d0c6c0f
ElderRamosAdv <elder.ramos.adv@gmail.com>
2026-04-10 16:48:14 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,12 @@ export const getStaticProps = async () => {
   }
 }
 
-export default function NotionDomainPage(props: PageProps) {
-  return <NotionPage {...props} />
+export default function Home() {
+  return (
+    <div style={{padding: 40, fontFamily: 'sans-serif'}}>
+      <h1>E. Ramos Advocacia</h1>
+      <p>Site em construção.</p>
+      <p>Em breve conteúdo completo.</p>
+    </div>
+  )
 }
