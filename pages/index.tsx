d6dbc470012fe3dd4c5dec5f90933623b2c4255e
ElderRamosAdv <elder.ramos.adv@gmail.com>
2026-04-10 16:55:10 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,8 @@
-import type { PageProps } from '@/lib/types'
-import { NotionPage } from '@/components/NotionPage'
-import { domain } from '@/lib/config'
-import { resolveNotionPage } from '@/lib/resolve-notion-page'
-
-export const getStaticProps = async () => {
-  try {
-    const props = await resolveNotionPage(domain)
-
-    return { props, revalidate: 10 }
-  } catch (err) {
-    console.error('page error', domain, err)
-
-    // we don't want to publish the error version of this page, so
-    // let next.js know explicitly that incremental SSG failed
-    throw err
-  }
-}
-
 export default function Home() {
   return (
-    <div style={{padding: 40, fontFamily: 'sans-serif'}}>
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
       <h1>E. Ramos Advocacia</h1>
-      <p>Site em construção.</p>
+      <p>Site em funcionamento.</p>
       <p>Em breve conteúdo completo.</p>
     </div>
   )
